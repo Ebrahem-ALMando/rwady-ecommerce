e4acca7f674c90bf9fc962aee215aa9a9d8276c9
Ibrahem <eb.almando@gamil.com>
2025-08-12 00:30:30 +0300

    test_v2

--- src/app/[locale]/(main)/(About Us)/about-us/page.jsx
+++ src/app/[locale]/(main)/(About Us)/about-us/page.jsx
@@ -1,76 +1,65 @@
-// import Navbar from "@/Components/Header/Navbar";
-// import Footer from "@/Components/Footer/Footer";
 import PageContainer from "@/Components/Shared/PageContainer/PageContainer";
-import { getLocale, getTranslations } from "next-intl/server";
+import { getTranslations } from "next-intl/server";
 import { getSettingData } from "@/utils/getSettingsData";
 import { extractSettingValue } from "@/utils/extractSettingValue";
-import { Suspense } from "react";
-import PageSkeleton from "@/Components/Shared/PageContainer/Skeleton/PageSkeleton";
 
-// export async function generateMetadata({ params }) {
-//     const { locale } = await params;
-//     const t = await getTranslations({ locale, namespace: "about" });
+export async function generateMetadata({ params }) {
+    const { locale } = await params;
+    const t = await getTranslations({ locale, namespace: "about" });
 
-//     const title = t("title");
-//     const description = t("description");
-//     const keywords = t("keywords");
+    const title = t("title");
+    const description = t("description");
+    const keywords = t("keywords");
 
-//     return {
-//         title,
-//         description,
-//         keywords,
-//         alternates: {
-//             canonical: "/about-us",
-//             languages: {
-//                 ar: "/ar/about-us",
-//                 en: "/en/about-us",
-//             }
-//         },
-//         openGraph: {
-//             title,
-//             description,
-//             url: `https://rwady.com/${locale}/about-us`,
-//             siteName: "Rwady",
-//             type: "article",
-//             locale,
-//         },
-//         twitter: {
-//             card: "summary_large_image",
-//             title,
-//             description,
-//             creator: "@rwady",
-//         },
-//         robots: {
-//             index: true,
-//             follow: true,
-//             googleBot: {
-//                 index: true,
-//                 follow: true,
-//             }
-//         },
-//         category: "About Us",
-//         icons: {
-//             icon: "/favicon.ico",
-//         }
-//     };
-// }
-
-export default function AboutUsPage() {
-    return (
-        <>
-            <Suspense fallback={<PageSkeleton />}>
-                <AboutUsPageData />
-            </Suspense>
-        </>
-    );
+    return {
+        title,
+        description,
+        keywords,
+        alternates: {
+            canonical: "/about-us",
+            languages: {
+                ar: "/ar/about-us",
+                en: "/en/about-us",
+            }
+        },
+        openGraph: {
+            title,
+            description,
+            url: `https://rwady.com/${locale}/about-us`,
+            siteName: "Rwady",
+            type: "article",
+            locale,
+        },
+        twitter: {
+            card: "summary_large_image",
+            title,
+            description,
+            creator: "@rwady",
+        },
+        robots: {
+            index: true,
+            follow: true,
+            googleBot: {
+                index: true,
+                follow: true,
+            }
+        },
+        category: "About Us",
+        icons: {
+            icon: "/favicon.ico",
+        }
+    };
 }
 
-async function AboutUsPageData() {
-    const locale = await getLocale();
-    const t = await getTranslations("about");
+export default async function AboutUsPage({ params }) {
+    const { locale } = await params;
+    const t = await getTranslations("shippingPolicy");
 
     const { settingData, initialError } = await getSettingData();
-    const content = extractSettingValue(settingData, `pages.about_us.${locale}`);
+    const content = extractSettingValue(
+        settingData,
+        `pages.about_us.${locale}`
+    );
 
     return (
         <>
